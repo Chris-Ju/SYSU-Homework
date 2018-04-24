e8eafd09f672e9e6cd66f22bcd42ef857503bc2c
ChrisJu <592931812@qq.com>
2018-04-25 00:49:36 +0800

    Homework

---
+++ MOSAD/HttpRequest/HttpRequest/MainPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Net.Http;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Newtonsoft.Json;
+using Windows.UI.Popups;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Windows.Data.Xml.Dom;
+using System.Text.RegularExpressions;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace HttpRequest
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        
+        public MainPage()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            this.InitializeComponent();
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            location.Text = "";
+            runner.Text = "";
+            ipsite.Text = "";
+            if (!Regex.IsMatch(number.Text, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$") && !Regex.IsMatch(number.Text, @"^[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$"))
+            {
+                var i = new MessageDialog("输入不合法").ShowAsync();
+                return;
+            }
+            queryAsyncPhone(number.Text);
+        }
+
+        async void queryAsyncPhone(string ip)
+        {
+            try
+            {
+
+                string url = "http://api.k780.com/?app=ip.get&ip=" + ip + "&appkey=33116&sign=31c6db9390065b2f0def78e073e4aebc&format=xml";
+                HttpClient client = new HttpClient();
+                string result = await client.GetStringAsync(url);
+                XmlDocument document = new XmlDocument();
+                document.LoadXml(result);
+                XmlNodeList list = document.GetElementsByTagName("att");
+                IXmlNode node = list.Item(0);
+                location.Text = node.InnerText;
+                list = document.GetElementsByTagName("operators");
+                node = list.Item(0);
+                runner.Text = node.InnerText;
+                list = document.GetElementsByTagName("ip");
+                node = list.Item(0);
+                ipsite.Text = node.InnerText;
+            }
+            catch
+            {
+                var i = new MessageDialog("该ip/域名不存在或未接入网络").ShowAsync();
+                return;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            weather.Text = "";
+            temperature.Text = "";
+            if (city.Text.Length == 0)
+            {
+                var i = new MessageDialog("输入不合法").ShowAsync();
+                return;
+            }
+            queryAsyncWeather(city.Text);
+        }
+
+        async void queryAsyncWeather(string city)
+        {
+            string url = "https://api.seniverse.com/v3/weather/now.json?key=cxatyah5f3gf9mhm&location=" + city;
+            HttpClient client = new HttpClient();
+            try
+            {
+                string result = await client.GetStringAsync(url);
+                string jsonStr = result.Substring(11, result.Length - 12);
+                JArray ja = (JArray)JsonConvert.DeserializeObject(jsonStr);
+                weather.Text = ja[0]["now"]["text"].ToString();
+                temperature.Text = ja[0]["now"]["temperature"].ToString();
+            }
+            catch
+            {
+                var i = new MessageDialog("输入不合法").ShowAsync();
+                return;
+            }
+        }
+    }
+}
---
+++ MOSAD/HttpRequest/README.md
@@ -0,0 +1,14 @@
+# HttpClient
+
+- 终于摆脱了 MyList，庆祝三秒， 1 2 3
+- 这次作业可以说是除了第一周之外最简单的一次了...虽然说想把 Http 各种东西学明白还是很困难的...但是只对于作业来说是很简单的...( 简单到没什么好说的 )
+
+- 首先推荐一个 [api 网站](www.nowapi.com) 里面提供了 xml 以及 json 的 API
+- 其次推荐一个 C# 处理 JSON 的插件 Newtonsoft.Json
+
+- 上效果图
+![](https://github.com/Chris-Ju/Picture/blob/master/HttpClient.jpg?raw=true)
+
+- 代码中的 API 账号去掉了，自己注册就好
+- [C# 解析 JSON 的方法](http://www.cnblogs.com/txw1958/archive/2012/08/01/csharp-json.html)
+
